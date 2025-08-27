e24194660316de47cb730d5e7bcfd8fc365c2b9b
Muhammad Junaid <junaidofficial678@gmail.com>
2025-08-27 16:45:20 +0500

    mongo url changed

--- config/db.js
+++ config/db.js
@@ -1,39 +1,28 @@
 import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGODB_URI);
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-  if (cached.conn) {
-    return cached.conn
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
+    };
+
+    if (!process.env.MONGODB_URI) {
+      throw new Error("❌ MONGODB_URI is not defined in the environment");
     }
 
-    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose => {
-      return mongoose
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn      
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => mongoose);
   }
 
-export default connectDB
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
